4b9b67de21350fbbe330989ab4572e898b27e3cc
Zhiar Piroti <zhiarsmp11@gmail.com>
2025-09-17 20:16:29 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frost",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.10",
+    description="Frost: A lightweight Python web framework built from scratch",
+    author="Zhiar Piroti",
+    include_package_data=True,
+)
