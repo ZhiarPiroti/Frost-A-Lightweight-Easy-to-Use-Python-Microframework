ea41a024cf995a5e8e5b922ef6c75b4545ecab49
Zhiar Piroti <zhiarsmp11@gmail.com>
2025-09-17 23:03:13 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="frost",
-    version="0.1.0",
+    name="pythonfrost",
+    version="0.1.1.1",
     packages=find_packages(),
-    install_requires=[],
-    python_requires=">=3.10",
-    description="Frost: A lightweight Python web framework built from scratch",
-    author="Zhiar Piroti",
     include_package_data=True,
+    install_requires=[], 
+    author="Zhiar Piroti",
+    author_email="zhiarsmp11@gmail.com",
+    description="A lightweight Python microframework for building fast and simple web apps.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZhiarPiroti/Frost-A-Lightweight-Easy-to-Use-Python-Microframework",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
